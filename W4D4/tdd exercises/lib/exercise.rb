e73ffb6299d07672bd7264c75d325097b6b6e477
tw <thomas.wang.design@gmail.com>
2020-06-25 17:36:08 -0700

    W4D4

---
+++ W4D4/tdd exercises/lib/exercise.rb
@@ -0,0 +1,28 @@
+class Array
+    def uniq
+        hash = {}
+        self.each do |ele|
+            hash[ele] = true
+        end
+        hash.keys
+    end
+
+    def two_sum
+        res = []
+        (0...self.length).each do |i|
+            (0...self.length).each do |j|
+                if j > i
+                    
+            end
+        end
+    end
+
+    def my_transpose
+    end
+    
+
+end
+
+def stock_picker(arr)
+    arr
+end
