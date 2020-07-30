140a0a1dbf7542f5a5c998cda86f7410fd705edc
tw <thomas.wang.design@gmail.com>
2020-07-30 02:41:27 -0700

    rake

--- W8D2/rails_lite/lib/session.rb
+++ W8D2/rails_lite/lib/session.rb
@@ -1,17 +1,15 @@
 require 'json'
 
 class Session
-  attr_reader :req
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
-    cookie = req.cookies('_rails_lite_app')
-    if cookie 
+    cookie = req.cookies['_rails_lite_app']
+    if cookie
       @data = JSON.parse(cookie)
     else
       @data = {}
     end
-
   end
 
   def [](key)
@@ -27,6 +25,5 @@ class Session
   def store_session(res)
     cookie = { path: '/', value: @data.to_json }
     res.set_cookie("_rails_lite_app", cookie)
-
   end
 end
